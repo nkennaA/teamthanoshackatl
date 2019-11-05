2099509e45678bf110c3359c521e68a18abeab2d
Lfry706 <lfry706@gmail.com>
2019-11-05 15:30:35 -0500

    update

--- GroupProject.js
+++ GroupProject.js
@@ -1,31 +1,8 @@
 
-
-document.getElementById("myBtn").addEventListener("click", validateForm);
-  
-  function validateForm() {
-    var x = document.forms["myForm"]["fname"].value;
-    if (x == "") {
-      alert("Name must be filled out");
-      return false;
-    }
-  } 
-
-/*function ValidateEmail(inputText)
-{
-var mailformat = /^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3})+$/;
-if(inputText.value.match(mailformat))
-{
-alert("You have entered a valid email address!");    //The pop up alert for a valid email address
-document.form1.text1.focus();
-return true;
+function validateForm() {
+  var x = document.forms["myForm"]["fname"].value;
+  if (x == "") {
+    alert("Name must be filled out");
+    return false;
+  }
 }
-else
-{
-alert("You have entered an invalid email address!");    //The pop up alert for an invalid email address
-document.form1.text1.focus();
-return false;
-}
-}
-
-xhttp.open("GET", "demo_get2.asp?fname=Henry&lname=Ford", true);
-xhttp.send(); 
