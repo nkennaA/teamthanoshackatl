e3f1354feb3e818f84d02510030c359d5829c864
nkennaA <nkenna@mit.edu>
2019-11-06 17:03:28 -0500

    wot

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 var app = express();
-    app.use(express.static('public'));
-
+    //app.use(express.static('public'));
+app.get('/', function (req, res) {
+    res.sendFile(__dirname+'/public/index.html');
+});
 app.listen(port, function () {
     console.log(`Example app listening on port`, port);
 });
