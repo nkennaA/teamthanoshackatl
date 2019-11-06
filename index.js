0de594f26ad7bc9af1a22534ca3ddf3d6e505ade
nkennaA <nkenna@mit.edu>
2019-11-06 16:11:27 -0500

    pl

--- index.js
+++ index.js
@@ -5,9 +5,6 @@ var path = require("path");
 
 var app = express();
     app.use(express.static(__dirname + '/public'));
-app.get('/', function(request, response){
-    response.render('public/index.html');
-});
 let port = process.env.PORT;
 if(port == null || port == ""){
     port = 8000;
