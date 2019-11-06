ade53a7fb7375906be3ada3955d0b2a879dc8dca
nkennaA <nkenna@mit.edu>
2019-11-06 16:57:56 -0500

    wee

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 var express = require("express");
 var app = express();
-var path = require("path");
-
+var port = process.env.PORT || 3000;
 
 var app = express();
     app.use(express.static('public'));
-let port = process.env.PORT;
-if(port == null || port == ""){
-    port = 8000;
-}
-app.listen(port);
-console.log('Server is running at ' + port);
+
+app.listen(port, function () {
+    console.log(`Example app listening on port`, port);
+});
