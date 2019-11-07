17c197daae0fa60889f6e36920cf9743eceab305
Lfry706 <lfry706@gmail.com>
2019-11-07 11:27:11 -0500

    new update

--- GroupProject.js
+++ GroupProject.js
@@ -1,23 +1,55 @@
 
-function validateForm() {
+
+
+function myFunction() {
+  
   var x = document.forms["myForm"]["fname"].value;
   var z = document.forms["myForm"]["email"].value;
+  var mailformat = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  var letters = /^[A-Za-z\s]+$/;
   
-  if (x == "") {
-    alert("Name must be filled out");
-    return false;
-  }
-  if(z ==""){
-    alert("Email must be filled out");
-    return false;
-  }
-  var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  if(z.value.match(mailformat)){
-    return true;
-  }else
+  if(x == "")
   {
-  alert("You have entered an invalid email address!");
-z.focus();
-return false;
+    document.myForm.email.focus();
+    alert ("message");
+    return false;
+  }
+  if(z == "")
+  {
+    document.myForm.email.focus();
+    alert('Message');
+    return false;
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  /*if (x.value.match(letters)) {
+   
+    return true;
+    
+   }
+   else{
+    alert("Name must be filled out")
+    return false;
+    
+   }
 }
+
+  if(z == "mailformat.test(str")){
+    document.myForm.email.focus();
+    return true;
+  }
+  else{
+    alert("You have entered an invalid email address!");
+    document.myForm.email.focus();
+    return false;*/
 }
+
