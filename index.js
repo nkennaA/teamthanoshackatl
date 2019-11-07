ec38003b0893da6e590a99ec81a94be8ca9cbc61
nkennaA <nkenna@mit.edu>
2019-11-07 15:11:02 -0500

    back to the basics

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ var express = require("express");
 var app = express();
 var port = process.env.PORT || 3000;
 const path = require('path');
-var mysql = require('mysql');
-var connection = port===3000 ? mysql.createConnection('mysql://r73j0ltyjn09jiza:gyulu0ynbz3hy3sp@d6q8diwwdmy5c9k9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/h5rumeh3i389hfbj')
-                             : mysql.createConnection(process.env.JAWSDB_URL);
-connection.connect();
+// var mysql = require('mysql');
+// var connection = port===3000 ? mysql.createConnection('mysql://r73j0ltyjn09jiza:gyulu0ynbz3hy3sp@d6q8diwwdmy5c9k9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/h5rumeh3i389hfbj')
+//                              : mysql.createConnection(process.env.JAWSDB_URL);
+// connection.connect();
 // export function pullEvents(){
 //     connection.query('SELECT * FROM the_events', (err,rows) => {
 //         if(err) {
