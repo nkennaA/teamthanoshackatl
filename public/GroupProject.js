28b5fae391f23a8eb72fb6778aaf5b16a2bd5fa2
nkennaA <nkenna@mit.edu>
2019-11-07 14:07:19 -0500

    woah

--- public/GroupProject.js
+++ public/GroupProject.js
@@ -25,30 +25,30 @@ connection.query('SELECT * FROM the_events', (err,rows) => {
   console.log(rows);
 });
 
-  function validateForm() {
-  var name1 = document.forms["myForm"]["fname"].value;
-  var email1 = document.forms["myForm"]["email"].value;
+//   function validateForm() {
+//   var name1 = document.forms["myForm"]["fname"].value;
+//   var email1 = document.forms["myForm"]["email"].value;
   
-  if (name1 == "") 
-  {
-    alert("Name must be filled out");
-    return false;
-  }
-  if (email1 == "")
-  {
-    alert("Email must be filled out");
-    return false;
-  }
-  return false;
-}
+//   if (name1 == "") 
+//   {
+//     alert("Name must be filled out");
+//     return false;
+//   }
+//   if (email1 == "")
+//   {
+//     alert("Email must be filled out");
+//     return false;
+//   }
+//   return false;
+// }
 
-function ValidateEmail(mail){
-  var emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  if (emailFormat.test(myForm.email.value)){
+// function ValidateEmail(mail){
+//   var emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+//   if (emailFormat.test(myForm.email.value)){
 
-  }
-  return true;
-}
-alert("You have entered an invalid email address!")
-return false;
-}
+//   }
+//   return true;
+// }
+// alert("You have entered an invalid email address!")
+// return false;
+// }
