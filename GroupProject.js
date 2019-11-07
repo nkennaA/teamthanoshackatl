48c5d0ca57ba2b00558cf344abdd479997542e9c
Lfry706 <lfry706@gmail.com>
2019-11-06 21:39:23 -0500

    update

--- GroupProject.js
+++ GroupProject.js
@@ -8,24 +8,16 @@ function validateForm() {
     return false;
   }
   if(z ==""){
-    var emailID = document.myForm.email.value;
-    atpos = emailID.indexOf("@");
-    dotpos = emailID.lastIndexOf(".");
     alert("Email must be filled out");
-
     return false;
   }
-
+  var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if(z.value.match(mailformat)){
+    return true;
+  }else
+  {
+  alert("You have entered an invalid email address!");
+z.focus();
+return false;
 }
-/*function validateEmail() {
-  var emailID = document.myForm.EMail.value;
-  atpos = emailID.indexOf("@");
-  dotpos = emailID.lastIndexOf(".");
-  
-  if (atpos < 1 || ( dotpos - atpos < 2 )) {
-     alert("Please enter correct email ID")
-     document.myForm.EMail.focus() ;
-     return false;
-  }
-  return( true );
-}*/
+}
