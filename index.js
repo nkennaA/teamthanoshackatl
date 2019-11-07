bc4f9a76146f65a0013d3574be8d7b4ec70de2f5
nkennaA <nkenna@mit.edu>
2019-11-06 22:22:50 -0500

    heroku plz

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.static('public'));
 //     res.render('./index.html');
 // });
 
-  app.get('/', (req, res) => {
+  app.get('*', (req, res) => {
     res.sendFile(path.resolve('index.html'));
   });
 app.listen(port, function () {
