a120aafa7004d188fdbb171379cff485fa9e4c22
nkennaA <nkenna@mit.edu>
2019-11-07 15:24:58 -0500

    plz heroku

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ var express = require("express");
 var app = express();
 var port = process.env.PORT || 3000;
 const path = require('path');
-if (process.env.JAWSDB_URL) {
- connection = mysql.createConnection(process.env.JAWSDB_URL);
-}
+
+var app = express();
 app.use(express.static(__dirname + '/public'));
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
