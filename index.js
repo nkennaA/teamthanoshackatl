56cd159094435687292cc7db595695b7c0b9bd33
nkennaA <nkenna@mit.edu>
2019-11-07 15:31:35 -0500

    woo

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const path = require('path');
 
 var app = express();
 app.use(express.static(__dirname + '/public'));
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
-//   });
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  });
 app.listen(port, function () {
     console.log('Example app listening on port', port);
 });
