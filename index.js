574d438d439d7702cdd60c28384176c3bf343d90
nkennaA <nkenna@mit.edu>
2019-11-07 15:22:26 -0500

    heroku plz

--- index.js
+++ index.js
@@ -3,25 +3,9 @@ var express = require("express");
 var app = express();
 var port = process.env.PORT || 3000;
 const path = require('path');
-var mysql = require("mysql");
-var connection;
 if (process.env.JAWSDB_URL) {
  connection = mysql.createConnection(process.env.JAWSDB_URL);
 }
-connection.connect((err) => {
- if(err){
-   console.log("Error Connecting");
-   return;
- }
- console.log("Connection Established");
-});
-connection.query('SELECT * FROM the_events', (err,rows) => {
- if(err) {
-   console.log("Error fetching rows");
- }
- console.log("Data received from Db:\n");
- console.log(rows);
-});
 app.use(express.static(__dirname + '/public'));
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
