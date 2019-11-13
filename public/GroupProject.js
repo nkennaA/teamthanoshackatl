c00588272f1fc11bbf0a363fd8b17770477e51de
Lfry706 <lfry706@gmail.com>
2019-11-13 11:21:22 -0500

    new

--- public/GroupProject.js
+++ public/GroupProject.js
@@ -1,49 +1,89 @@
 
 
-function validateForm()
-{
+function validateForm() {
   const 
     myEmail = document.getElementById("mail").value,
     fName = document.getElementById("fname").value,
     lName = document.getElementById("lname").value,
-    nameReg = /^[a-zA-Z' `-]+$/ ,
-    emailReg = /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/,      
+    nameReg =  /^[A-Z][a-zA-Z-']+[a-zA-Z]$/, 
+    emailReg = /^[a-zA-Z0-9][a-zA-Z0-9!@\$%\&-_.]+[\S+@\S]+@(yahoo|ymail|rocketmail|gmail|aol|hotmail|icloud|mailchimp|zoho|gmx|hushmail|mail|outlook|yandex|army|godaddy|aemail4u|boardermail|caramail|email|fastmail|swissmail|comcast|domain|namecheap|name|msn|theclubhou.se|simplicity|indeed|bluehost)\.(net|mil|com|in|org|co\.uk)$/i,  
+    spcChar= /^[0-9!@#\$%\^\&*\)\(+=._-]+$/, 
+    errChar= /^\S+$/,
+    emailErr= /^\S+$/,
+    charEmail= emailErr.test(myEmail), 
+    myChar1= errChar.test(fName),
+    myChar2= errChar.test(lName),
+    char1= spcChar.test(fName),
+    char2= spcChar.test(lName),    
     nameRes = nameReg.test(fName),
     nameRes2 = nameReg.test(lName),
     emailRes = emailReg.test(myEmail);
-    
-    
-    if(fName == "")
-  {
-    alert("Please Enter a First Name")
+   
+  if(fName == "") {
+    alert("Please Enter Your First Name!");
     }
-  else if(nameRes === false)
-  {
-    alert("Please Enter a Valid First Name");
-     }
-  if(lName == "")
-  {
-    alert("Please Enter a Last Name");
-    }
-  else if(nameRes2 === false)
-  {
-    alert("Please Enter a Valid Last Name");
-    }
-  if(myEmail == "")
-  {
-    alert("Please Enter a Email Address");
-    return false;
-  }
-  else if(emailRes === false)
-  {
-    alert("Please Enter a Valid Email Address");
+  
+  else if(myChar1 == false){
+    alert("Do Not Put Spaces In First Name!");
     return false;
     }
-  if(emailRes === true && nameRes2 === true && nameRes ===true){
+  
+  else if(fName[0] !== fName[0].toUpperCase()) {
+      alert("Please Capitalize Your First Name!");
+      return false;
+    }
+  
+  else if(char1 === true && char1 !== nameRes && nameRes !== spcChar) {
+      alert("Do Not Add Special Characters In Your First Name!");
+      return true;
+    }
+   
+  else if(nameRes === false) {
+    alert("Please Enter a Valid First Name!");
+    return false;
+    }
+  
+  if(lName == "") {
+    alert("Please Enter Your Last Name!");
+    }
     
-    return confirm("Thank You for signing in @ The Clubhou.se!!!");
-
+  else if(myChar2 == false) {
+      alert("Do Not Put Spaces in Last Name!");
+      return false;
+    }
+    
+  else if(lName[0] !== lName[0].toUpperCase()) {
+      alert("Please Capitalize Your Last Name!");
+      return false;
+    }
+    
+  else if(char2 === true && char2 !== nameRes2 && nameRes2 !== spcChar) {
+      alert("Do Not Add Special Characters In Your Last Name!");
+      return true;
+    }
+  
+  else if(nameRes2 === false) {
+    alert("Please Enter a Valid Last Name!");
+    return false;
+    }
+    
+  if(myEmail == "") {
+    alert("Please Enter Your Email Address!");
+    
+    }
+  else if(charEmail !== true) {
+    alert("Please Don't Add Any Spaces In Your Email!");
+    return true;
   }
+  
+  else if(emailRes === false) {
+    alert("Please Enter a Valid Email Address!");
+    return false;
+    }
+  
+  else if(emailRes === true && nameRes2 === true && nameRes ===true) {
+    return confirm("Thank You For Signing Into An Event @ The Clubhou.se!!!");
+    }
 }
    
 console.log("woo");
