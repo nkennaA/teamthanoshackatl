9ba5e70b9461c7c32865b809ffdc6ad6ce07d5d3
FIRST_NAME LAST_NAME <amlockhart24@gmail.com>
2019-11-13 14:52:50 -0500

    pushing

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-var express = require("express");
-var app = express();
-var port = process.env.PORT || 3000;
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
 const path = require('path');
-app.use(express.static(__dirname + '/public'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-  });
+const app = express();
+
+const {getHomePage}=require('./routes/homePage.js');
+if(process.env.JAWSDB_URL) {
+ var connection = mysql.createConnection(process.env.JAWSDB_URL);
+ connection.connect(function(err){
+   if(err) {
+     console.log(err);
+   }
+   else {
+     console.log("connected");
+   }
+ });
+} else {
+ console.log("NO JAWS URL");
+}
+global.connection = connection;
+var port = process.env.PORT || 3000;
+app.set('port', process.env.port || port);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(fileUpload());
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', __dirname + '/views');
+
+app.get('/', getHomePage);
+
 app.listen(port, function () {
-    console.log('Example app listening on port', port);
+   console.log('Example app listening on port', port);
 });
