02a231c4db130c3ffd044f18f16b3bc386d01360
nkennaA <nkenna@mit.edu>
2019-11-13 13:51:45 -0500

    WOO

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-var express = require("express");
-var app = express();
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+const path = require('path');
+const app = express();
+
+if(process.env.JAWS_DB_URL) {
+  var connection = mysql.createConnection(process.env.JAWS_DB_URL);
+  connection.connect(function(err){
+    if(err) {
+      console.log(err);
+    }
+    else {
+      console.log("connected");
+    }
+  });
+}
+global.connection = connection;
+
 var port = process.env.PORT || 3000;
 const path = require('path');
 app.use(express.static(__dirname + '/public'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-  });
 app.listen(port, function () {
     console.log('Example app listening on port', port);
 });
